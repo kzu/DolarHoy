06f394a5a1776de0b7e9d3abfc8d079a74c012c5
Daniel Cazzulino <daniel@cazzulino.com>
2016-03-22 10:08:55 -0300

    Add datetime rendering.

--- DolarHoy/index.ashx.cs
+++ DolarHoy/index.ashx.cs
@@ -3,6 +3,7 @@ using System.Web;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Linq;
+using System;
 
 namespace DolaryHoy
 {
@@ -19,7 +20,6 @@ namespace DolaryHoy
 			string compra = data.CasaCambioCompraValue;
 			string venta = data.CasaCambioVentaValue;
 
-
 			if (context.Request.AcceptTypes.Any(s => s == "application/json") || 
 				context.Request.QueryString.AllKeys.Contains("json") ||
 				(context.Request.QueryString.Count != 0 && context.Request.QueryString.GetValues (0).Any (s => s == "json"))) {
@@ -31,7 +31,14 @@ namespace DolaryHoy
 				context.Response.AddHeader ("Access-Control-Allow-Origin", "*");
 				((JObject)result).WriteTo (new JsonTextWriter (context.Response.Output) { Formatting = Formatting.Indented });
 			} else {
-				context.Response.Write (string.Format ("Compra: {0}, Venta: {1}", compra, venta));
+				var addDate = context.Request.QueryString.AllKeys.Contains("date") ||
+					(context.Request.QueryString.Count != 0 && context.Request.QueryString.GetValues (0).Any (s => s == "date"));
+
+				if (addDate)
+					context.Response.Write (string.Format ("Compra: {0}, Venta: {1} ({2})", compra, venta,
+						TimeZoneInfo.ConvertTimeFromUtc (DateTime.UtcNow, TimeZoneInfo.CreateCustomTimeZone ("GMT-0300", new TimeSpan (-3, 0, 0), "GMT-0300", "GMT-0300")).ToString ()));
+				else
+					context.Response.Write (string.Format ("Compra: {0}, Venta: {1}", compra, venta));
 			}
 		}
 
