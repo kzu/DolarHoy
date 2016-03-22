689e94e306242fc552249058433e2f13e5d29892
Daniel Cazzulino <daniel@cazzulino.com>
2016-03-22 02:31:00 -0300

    Version inicial de dolarhoy simple

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # DolarHoy
+
 Una forma conveniente de chequear el valor del dolar hoy
+
+Uso:
+
+* [http://dolarhoy.azurewebsites.net](http://dolarhoy.azurewebsites.net): devuelve valor de compra y venta como texto
+* [http://dolarhoy.azurewebsites.net/?json](http://dolarhoy.azurewebsites.net/?json): devuelve valor de compra y venta como json
+
+Este servicio combinado con Azure Logic Apps genera los tweets de [@dolarhoybot](http://twitter.com/dolarhoybot).
---
+++ index.ashx.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+using System.Web;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+
+namespace DolaryHoy
+{
+	public class Index : IHttpHandler
+	{
+		public void ProcessRequest (HttpContext context)
+		{
+			var json = new WebClient().DownloadString("http://contenidos.lanacion.com.ar/json/dolar");
+			json = json.Substring (
+				json.IndexOf ('{'),
+				json.LastIndexOf ('}') - json.IndexOf ('{') + 1);
+
+			dynamic data = JObject.Parse(json);
+			string compra = data.CasaCambioCompraValue;
+			string venta = data.CasaCambioVentaValue;
+
+
+			if (context.Request.AcceptTypes.Any(s => s == "application/json") || 
+				context.Request.QueryString.AllKeys.Contains("json") ||
+				(context.Request.QueryString.Count != 0 && context.Request.QueryString.GetValues (0).Any (s => s == "json"))) {
+				dynamic result = new JObject();
+				result.compra = compra;
+				result.venta = venta;
+
+				context.Response.ContentType = "application/json";
+				context.Response.AddHeader ("Access-Control-Allow-Origin", "*");
+				((JObject)result).WriteTo (new JsonTextWriter (context.Response.Output) { Formatting = Formatting.Indented });
+			} else {
+				context.Response.Write (string.Format ("Compra: {0}, Venta: {1}", compra, venta));
+			}
+		}
+
+		public bool IsReusable => true;
+	}
+}
